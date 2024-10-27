7f3426ef5c8782f3dfc073ea5038588065e87bb4
Alan-Sodea <konzuosodea@gmail.com>
2024-10-27 19:26:37 +0100

    vercel config file commit

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
-
 const router = require("./Routes/index")
-
-app.use(express.json());  // to parse json data
-
-app.use(cors({origin : "*"}));
 
 (async () => {
     mongoose.connect("mongodb://localhost:27017/userDB");
 
-    app.use("/api", router);
-    app.get("/", (req, res) => res.send("Welcome to the api server"));
-    app.listen(3000, () => {
-        console.log("listening on port 3000...");
-    })
+})
 
+app.use(express.json());  // to parse json data
+app.use(cors({origin : "*"}));
+app.get("/", (req, res) => res.send("Welcome to the api server"));
+
+app.use("/api", router);
+
+app.listen(3000, () => {
+    console.log("listening on port 3000...");
 })()
 
 module.exports = app;
